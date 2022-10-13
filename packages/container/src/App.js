a9662cbc1b42be679c5a4e3138fddadaf1ce0832
nitin.fed@gmail.com <nitin.fed@gmail.com>
2022-10-12 21:46:19 -0400

    m

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -16,8 +16,6 @@ const App = () => {
 
 export default App;
 
-
-
 // - uses: shinyinc/action-aws-cli@v1.2
 // - run: aws s3 sync dist s3://${{ secrets.AWS_S3_BUCKET_NAME }}/container/latest
 //   env:
